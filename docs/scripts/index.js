b4429ba9ddfa987cb9879a989c8ba1e5523de93c
neslihva <70858706+neslihva@users.noreply.github.com>
2024-02-09 05:11:14 +0100

    html css

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -6,29 +6,3 @@ fetch('me.json').then(function (response) {
     console.error('Something went wrong with retrieving me!');
     console.error(error);
 });
-
-let swiperCards = new Swiper(".card__content", {
-    loop: true,
-    spaceBetween: 32,
-    grabCursor: true,
-  
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-  
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  
-    breakpoints:{
-      600: {
-        slidesPerView: 2,
-      },
-      968: {
-        slidesPerView: 3,
-      },
-    },
-  });
