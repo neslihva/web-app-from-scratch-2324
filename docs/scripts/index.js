e8fc0b0635349fd087111a0a8461aff200347bcc
neslihva <70858706+neslihva@users.noreply.github.com>
2024-02-12 11:57:33 +0100

    html css js update

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,8 +1,50 @@
-fetch('me.json').then(function (response) {
-    return response.json();
-}).then(function (obj) {
-    console.log(obj);
-}).catch(function (error) {
-    console.error('Something went wrong with retrieving me!');
-    console.error(error);
-});
+async function postJSON(data) {
+    try {
+      const response = await fetch("./me.json")
+  
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  
+  const data = { username: "neslihva" };
+  postJSON(data);
+
+  const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
